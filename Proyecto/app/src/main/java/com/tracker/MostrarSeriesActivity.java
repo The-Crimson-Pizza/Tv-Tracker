7476b782ca92ea3ccf0b31fc0aab6efa58e59718
Andrea Lobo <andrea.lobo93@gmail.com>
2020-02-25 17:17:26 +0100

    Añadir listview de Series (general)

---
+++ Proyecto/app/src/main/java/com/tracker/MostrarSeriesActivity.java
@@ -0,0 +1,14 @@
+package com.tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarSeriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_series);
+    }
+}
