58bb689e65dfbfa189046d041b1b7bb4d9898b38
Andrea Lobo <andrea.lobo93@gmail.com>
2020-03-11 17:06:46 +0100

    Arreglar vainas

--- Proyecto/app/src/main/java/com/tracker/ui/home/HomeViewModel.java
+++ Proyecto/app/src/main/java/com/tracker/ui/home/HomeViewModel.java
@@ -9,8 +9,8 @@ public class HomeViewModel extends ViewModel {
     private MutableLiveData<String> mText;
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+//        mText = new MutableLiveData<>();
+//        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
