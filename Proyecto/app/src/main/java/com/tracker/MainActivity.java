4df94fd41c485a85b6e17090a5490db0a6687b74
aaron <bestbudgetphone2017001@gmail.com>
2020-03-24 11:48:13 +0100

    activity series done

--- Proyecto/app/src/main/java/com/tracker/MainActivity.java
+++ Proyecto/app/src/main/java/com/tracker/MainActivity.java
@@ -2,6 +2,7 @@ package com.tracker;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -42,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, DetallesSerie.class);
+                startActivity(intent);
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
