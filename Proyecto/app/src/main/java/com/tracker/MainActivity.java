f4f44eb30c47e178756d8a11be3972cc90d8f8db
Andrea Lobo <andrea.lobo93@gmail.com>
2020-03-24 18:03:16 +0100

    fusion con aaron

--- Proyecto/app/src/main/java/com/tracker/MainActivity.java
+++ Proyecto/app/src/main/java/com/tracker/MainActivity.java
@@ -43,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
                 Intent intent = new Intent(MainActivity.this, DetallesSerie.class);
                 startActivity(intent);
-                Intent intent = new Intent(MainActivity.this, DescripcionSerie.class);
-                 startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, DescripcionSerie.class);
+//                 startActivity(intent);
 
             }
         });
