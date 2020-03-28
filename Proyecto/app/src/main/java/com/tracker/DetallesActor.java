5dc1e371e5a1e31e4a5a486a7458ed45e85f88b3
aaron <aaron.perez@juanxxiii.net>
2020-03-28 02:56:13 +0100

    insercion de activity de actor

---
+++ Proyecto/app/src/main/java/com/tracker/DetallesActor.java
@@ -0,0 +1,64 @@
+package com.tracker;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class DetallesActor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_actor);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // SE AÑADE EL BOTÓN DE ATRÁS AL TOOLBAR
+        // toolbar.setNavigationIcon(R.drawable.arrow);
+
+        setTitle("Alejandro Amenábar");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detalles_actor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_share) {
+
+            return true;
+        }else if(id == R.id.action_wiki){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
--- Proyecto/app/src/main/java/com/tracker/DetallesSerie.java
+++ Proyecto/app/src/main/java/com/tracker/DetallesSerie.java
@@ -1,5 +1,6 @@
 package com.tracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -28,6 +29,8 @@ public class DetallesSerie extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(DetallesSerie.this, DetallesActor.class);
+                startActivity(intent);
             }
         });
     }
