3e1a25850059dc6e2d70fe4eb52fb158750776ae
aaron <aaron.perez@juanxxiii.net>
2020-03-28 13:15:27 +0100

    datos de actor insertadosy reestructuracion de content

--- Proyecto/app/src/main/java/com/tracker/DetallesActor.java
+++ Proyecto/app/src/main/java/com/tracker/DetallesActor.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
--- Proyecto/app/src/main/java/com/tracker/ui/home/HomeFragment.java
+++ Proyecto/app/src/main/java/com/tracker/ui/home/HomeFragment.java
@@ -59,7 +59,6 @@ public class HomeFragment extends Fragment {
             }
         });
 
-
         return root;
     }
 }
